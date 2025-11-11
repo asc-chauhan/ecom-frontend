ea8e92f37ff9c820e340a554e8efd2026667e964
asc-chauhan <anshulasc151976@gmail.com>
2025-11-11 18:44:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { FaBeer } from 'react-icons/fa'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 class="flex items-center justify-center h-screen bg-gray-800 text-white
+      text-2xl font-bold">
+        Welcome! <FaBeer/>
+      </h1>
+    </>
+  )
+}
+
+export default App
