1e9d9cee2c680dfc0b54e75e98f54315ee8ffe41
asc-chauhan <anshulasc151976@gmail.com>
2026-01-27 14:24:50 +0530

    stripe integrated and tested the flow

--- src/components/checkout/StripePayment.jsx
+++ src/components/checkout/StripePayment.jsx
@@ -12,11 +12,23 @@ const StripePayment = () => {
   const { clientSecret } = useSelector((state) => state.auth);
   const { totalPrice } = useSelector((state) => state.carts);
   const { isLoading, errorMessage } = useSelector((state) => state.errors);
+  const { user, selectedUserCheckoutAddress } = useSelector((state) => state.auth);
 
   const dispatch = useDispatch();
   useEffect(() => {
     if(!clientSecret){
-      dispatch(createStripePaymentSecret(totalPrice));
+      const sendData = {
+        amount: Number(totalPrice) * 100,
+        currency: "usd",
+        email: user.email,
+        name: `${user.username}`,
+        address: selectedUserCheckoutAddress,
+        description: `Order for ${user.email}`,
+        metadata: {
+          test: "1"
+        }
+      }
+      dispatch(createStripePaymentSecret(sendData));
     }
   }, [clientSecret]);
 
